8c978845722c548007cf8800cbcce99c0a1d90d9
Max Eckardt <max@cas.au.dk>
2020-04-02 10:31:50 +0200

    Update vFooter.vue

--- src/components/vFooter.vue
+++ src/components/vFooter.vue
@@ -1,11 +1,14 @@
 <template>
-  <footer class="page__footer footer">
-    <object class="page__footer__logo" role="img" type="image/svg+xml" aria-label="The logo of Aarhus Universtity" tabindex="-1">
-      <p>The logo of Aarhus University.</p>
-    </object>
-    <object class="page__footer__seal" role="img" type="image/svg+xml" aria-label="The seal of Aarhus Universtity" tabindex="-1">
-      <p>The seal of Aarhus University. The seal depicts an anchor surrounded by two dolphins. The university motto and Latin name are written on the outline: Solidum petit in profundis - Universitas Arhusiensis.</p>
-    </object>
+  <footer class="footer">
+    <div class="page__footer">
+
+      <object class="page__footer__logo" role="img" type="image/svg+xml" aria-label="The logo of Aarhus Universtity" tabindex="-1">
+        <p>The logo of Aarhus University.</p>
+      </object>
+      <object class="page__footer__seal" role="img" type="image/svg+xml" aria-label="The seal of Aarhus Universtity" tabindex="-1">
+        <p>The seal of Aarhus University. The seal depicts an anchor surrounded by two dolphins. The university motto and Latin name are written on the outline: Solidum petit in profundis - Universitas Arhusiensis.</p>
+      </object>
+    </div>
   </footer>
 </template>
 
